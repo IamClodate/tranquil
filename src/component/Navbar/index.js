41b7f50fb7f4da068330909144271944ff79d2c3
IamClodate <clodatemnisi@gmail.com>
2023-11-13 20:48:30 +0200

    added the contents  name of the navbars menu

--- src/component/Navbar/index.js
+++ src/component/Navbar/index.js
@@ -2,9 +2,36 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>Navbar</h1>
-    </div>
+    <>
+        <Nav>
+            <NavLink to="/">
+                <img src="file:///C:/Users/Clodate/Downloads/tranquilquest-tours-high-resolution-logo.svg" alt=""/>
+                <h1>This will be th</h1>
+            </NavLink>
+            <Bars />
+            <NavMenu>
+                <NavLink to="/about" activeStyle>
+                    About
+                </NavLink>
+                <NavLink to="/about" activeStyle>
+                    About
+                </NavLink>
+                <NavLink to="/services" activeStyle>
+                    Services
+                </NavLink>
+                <NavLink to="/tours" activeStyle>
+                    Tours
+                </NavLink>
+                <NavLink to="/blog" activeStyle>
+                    Blog
+                </NavLink>
+                <NavLink to="/contact" activeStyle>
+                    Contact
+                </NavLink>
+            </NavMenu>
+        </Nav>
+        
+    </>
   )
 }
 
