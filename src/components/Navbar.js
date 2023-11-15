7ad42c1c236bbb9976409adf09bd83c02626dd40
IamClodate <clodatemnisi@gmail.com>
2023-11-15 13:19:37 +0200

    added all the neccessary link on Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { useState,   useEffect } from 'react';
+import { Link } from "react-router-dom";
+import * as Icons from "react-icons/fa";
+import "./Navbar.css";
+import Button from "./Button";
+import { navItems } from "./NavItems.js";
+
+function Navbar() {
+  return (
+    <>
+        <nav className="navbar"></>    
+    </>
+  );
+}
+
+export default Navbar;
