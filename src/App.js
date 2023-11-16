011d388b0fda89f2bad07a16c8e561e39a0995ec
IamClodate <clodatemnisi@gmail.com>
2023-11-16 09:30:13 +0200

    deleted the import from svg from the App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+
 
 
 function App() {
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,10 +3,15 @@ import {Link} from "react-router-dom"
 
 function Button() {
   return (
+    <>
     <Link to="signup">
-    <button className="btn">Sign Up</button>
+      <button className="btn">
+        <Icon.FaUserPlus/>
+        <span>Sign Up</span>
+      </button>
     </Link>
-  )
+    </>
+  );
 }
 
 export default Button;
