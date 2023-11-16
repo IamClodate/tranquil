c21cca195d959c829045f1dda0bd6e70e7738247
IamClodate <clodatemnisi@gmail.com>
2023-11-16 23:36:08 +0200

    added all the imports to take you to the designated pages when you click on the navbars menus

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from "./pages/Home";
+import Contacts from "./pages/Contacts";
+import Products from "./pages/Products";
+import Services from "./pages/Services";
+import Signup from "./pages/Signup";
 
 
 
