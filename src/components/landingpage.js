903cc894b043eebc4371a37312042deb27bdf7a2
IamClodate <clodatemnisi@gmail.com>
2023-11-17 09:44:32 +0200

    added the  home page heading and the paragraph welcoming message

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -8,6 +8,21 @@ const landingpage = () => {
         <video src={BgVideo} autoPlay muted loop class="video-bg" />
         <div className="bg-overlay"></div>
 
+        <div className="navbar">
+            <div className="menu">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+            <div className="home-text">
+                <h1>The Bubu Island</h1>
+                <p>Come live out your ideal vacationb with us</p>
+
+            </div>
+
+        </div>
+
     </div>
   )
 }
